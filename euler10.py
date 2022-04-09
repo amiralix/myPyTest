9d99d8714fe03bd0c622d7a5f0a6bdf4ac3d4892
amiralex <saazimi@sic.net>
2022-04-09 17:18:01 +0430

    202204092

---
+++ euler10.py
@@ -0,0 +1,20 @@
+def check_prime(number):
+    if(number == 2 or number ==3 or number ==5 or number == 7):
+        return True
+    # if int(number**0.5) == 2:
+    #     return False
+    for factor in range(2,int(number**0.5)+1):
+        if(number % factor == 0):
+            return False
+    return True
+
+
+sum = 0
+for counter in range(2,2000001):
+    if(check_prime(counter) == True):
+        sum += counter
+
+print(sum)
+
+# for i in range (2,11):
+#     print(i , check_prime(i))
